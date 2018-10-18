a1b41a45062a4f18e9eee1d2aeed5870d8efdf67
Ajay Viswanathan <ajayviswanathan@mediaiqdigital.com>
2018-10-19 02:55:03 +0530

    Adds test cases for json object messages

--- notification-utils-core/src/test/scala/com/miq/caps/EmailNotificationSpec.scala
+++ notification-utils-core/src/test/scala/com/miq/caps/EmailNotificationSpec.scala
@@ -3,7 +3,9 @@ package com.miq.caps
 import com.miq.caps.generators.EmailMessageGenerator
 import com.miq.caps.utils.JsonUtils
 import com.miq.caps.utils.notification.context.EmailContext
+import com.miq.caps.utils.notification.email.EmailMessage
 import com.miq.caps.utils.notification.verticle.EmailNotificationVerticle
+import io.vertx.core.json.JsonObject
 import org.scalacheck.Prop
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
@@ -18,11 +20,48 @@ class EmailNotificationSpec extends VertxSpec[EmailNotificationVerticle] with Ma
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
     PropertyCheckConfiguration(minSize = 1, sizeRange = 10, minSuccessful = 5, workers = 1)
 
-  "EmailNotificationVerticle" should "simulate receive messages and send Email" in {
+  private def generateJsonObjectMessage(message: EmailMessage): JsonObject = {
+    new JsonObject()
+      .put("subject", message.subject)
+      .put("recipient", message.recipient)
+      .put("text", message.text)
+  }
+
+  "EmailNotificationVerticle" should "generate messages and simulate send Email" in {
     val test = Prop.forAll(EmailMessageGenerator.genEmailMessage(EmailContext.sender)) { message =>
       val sender = for {
         response <- vertx.eventBus()
           .sendFuture[String](EmailNotificationVerticle.SIMULATE_EMAIL_STRING_MESSAGE, JsonUtils.encode(message))
+        decoded <- Future.fromTry(JsonUtils.decodeAsTry[ResponseStatus](response.body))
+      } yield decoded
+
+      if (sender.futureValue == ValidResponse) Prop.passed
+      else Prop.falsified
+    }
+    test.check(_.withMinSuccessfulTests(5))
+    Future.successful(Assertions.succeed)
+  }
+
+  it should "generate messages and send Email" in {
+    val test = Prop.forAll(EmailMessageGenerator.genEmailMessage(EmailContext.sender)) { message =>
+      val sender = for {
+        response <- vertx.eventBus()
+          .sendFuture[String](EmailNotificationVerticle.EMAIL_JSON_OBJECT, generateJsonObjectMessage(message))
+        decoded <- Future.fromTry(JsonUtils.decodeAsTry[ResponseStatus](response.body))
+      } yield decoded
+
+      if (sender.futureValue == ValidResponse) Prop.passed
+      else Prop.falsified
+    }
+    test.check(_.withMinSuccessfulTests(1))
+    Future.successful(Assertions.succeed)
+  }
+
+  it should "simulate receive messages and send Email" in {
+    val test = Prop.forAll(EmailMessageGenerator.genEmailMessage(EmailContext.sender)) { message =>
+      val sender = for {
+        response <- vertx.eventBus()
+          .sendFuture[String](EmailNotificationVerticle.SIMULATE_EMAIL_JSON_OBJECT, generateJsonObjectMessage(message))
         decoded <- Future.fromTry(JsonUtils.decodeAsTry[ResponseStatus](response.body))
       } yield decoded
 
--- notification-utils-core/src/test/scala/com/miq/caps/SlackNotificationSpec.scala
+++ notification-utils-core/src/test/scala/com/miq/caps/SlackNotificationSpec.scala
@@ -3,6 +3,7 @@ package com.miq.caps
 import com.miq.caps.generators.SlackMessageGenerator
 import com.miq.caps.utils.JsonUtils
 import com.miq.caps.utils.notification.verticle.SlackNotificationVerticle
+import io.vertx.core.json.JsonObject
 import org.scalacheck.Prop
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
@@ -17,7 +18,12 @@ class SlackNotificationSpec extends VertxSpec[SlackNotificationVerticle] with Ma
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
     PropertyCheckConfiguration(minSize = 1, sizeRange = 10, minSuccessful = 5, workers = 1)
 
-  "SlackNotificationVerticle" should "simulate receive messages and publish to slack" in {
+  private def generateJsonObjectMessage(text: String): JsonObject = {
+    new JsonObject()
+      .put("text", text)
+  }
+
+  "SlackNotificationVerticle" should "generate string messages and simulate publish to slack" in {
     val test = Prop.forAll(SlackMessageGenerator.genStringMessage) { message =>
       val sender = for {
         response <- vertx.eventBus()
@@ -32,11 +38,41 @@ class SlackNotificationSpec extends VertxSpec[SlackNotificationVerticle] with Ma
     Future.successful(Assertions.succeed)
   }
 
-  it should "receive messages and publish to slack" in {
+  it should "generate string messages and publish to slack" in {
     val test = Prop.forAll(SlackMessageGenerator.genStringMessage) { message =>
       val sender = for {
         response <- vertx.eventBus()
           .sendFuture[String](SlackNotificationVerticle.PUBLISH_STRING_TO_WEBHOOK, message)
+        decoded <- Future.fromTry(JsonUtils.decodeAsTry[ResponseStatus](response.body))
+      } yield decoded
+
+      if (sender.futureValue == ValidResponse) Prop.passed
+      else Prop.falsified
+    }
+    test.check(_.withMinSuccessfulTests(1))
+    Future.successful(Assertions.succeed)
+  }
+
+  it should "simulate json object messages and simulate publish to slack" in {
+    val test = Prop.forAll(SlackMessageGenerator.genStringMessage) { message =>
+      val sender = for {
+        response <- vertx.eventBus()
+          .sendFuture[String](SlackNotificationVerticle.SIMULATE_PUBLISH_JSON_OBJECT_TO_WEBHOOK, generateJsonObjectMessage(message))
+        decoded <- Future.fromTry(JsonUtils.decodeAsTry[ResponseStatus](response.body))
+      } yield decoded
+
+      if (sender.futureValue == ValidResponse) Prop.passed
+      else Prop.falsified
+    }
+    test.check(_.withMinSuccessfulTests(5))
+    Future.successful(Assertions.succeed)
+  }
+
+  it should "generate json object messages and publish to slack" in {
+    val test = Prop.forAll(SlackMessageGenerator.genStringMessage) { message =>
+      val sender = for {
+        response <- vertx.eventBus()
+          .sendFuture[String](SlackNotificationVerticle.PUBLISH_JSON_OBJECT_TO_WEBHOOK, generateJsonObjectMessage(message))
         decoded <- Future.fromTry(JsonUtils.decodeAsTry[ResponseStatus](response.body))
       } yield decoded
 
