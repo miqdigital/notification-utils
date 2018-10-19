8b76da5fd9d848e115264a245537c4a4d3973f44
Ajay Viswanathan <ajayviswanathan@mediaiqdigital.com>
2018-10-19 17:44:29 +0530

    Adds slack bot message domain

--- notification-utils-commons/src/main/scala/com/miq/caps/utils/notification/slack/SlackBotMessage.scala
+++ notification-utils-commons/src/main/scala/com/miq/caps/utils/notification/slack/SlackBotMessage.scala
@@ -1,5 +1,11 @@
 package com.miq.caps.utils.notification.slack
 
-class SlackBotMessage {
+import io.circe.generic.semiauto._
+import io.circe.{Decoder, Encoder}
 
+case class SlackBotMessage(id: String, message: SlackMessage)
+
+object SlackBotMessage {
+  implicit val encoder: Encoder[SlackBotMessage] = deriveEncoder
+  implicit val decoder: Decoder[SlackBotMessage] = deriveDecoder
 }
--- notification-utils-commons/src/test/scala/com/miq/caps/generators/SlackMessageGenerator.scala
+++ notification-utils-commons/src/test/scala/com/miq/caps/generators/SlackMessageGenerator.scala
@@ -1,15 +1,29 @@
 package com.miq.caps.generators
 
-import com.miq.caps.utils.notification.slack.SlackMessage
+import com.miq.caps.utils.notification.slack.{SlackBotMessage, SlackMessage}
 import org.scalacheck.Gen
 
 object SlackMessageGenerator {
 
-  val genStringMessage: Gen[String] =
-    Gen.alphaLowerStr.suchThat(_.length > 0)
+  val genNonEmptyString: Gen[String] = Gen.alphaLowerStr.suchThat(_.length > 0)
 
   val genSlackMessage: Gen[SlackMessage] = for {
-    text <- genStringMessage
+    text <- genNonEmptyString
   } yield SlackMessage(text)
+
+  val genEmail: Gen[String] = for {
+    username <- genNonEmptyString
+    domain <- genNonEmptyString
+  } yield s"$username@$domain.com"
+
+  val genSlackUserBotMessage: Gen[SlackBotMessage] = for {
+    id <- genEmail
+    message <- genSlackMessage
+  } yield SlackBotMessage(id, message)
+
+  val genSlackIdBotMessage: Gen[SlackBotMessage] = for {
+    id <- genNonEmptyString
+    message <- genSlackMessage
+  } yield SlackBotMessage(id, message)
 
 }
